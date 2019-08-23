2e0ab6accdd320313451f3866c930e9817318066
yuanqiaoming <yuanqiaoming@yuanqiaominggwb>
2019-08-23 14:06:58 +0800

    增加一些

--- microservicecloud-provider/src/main/java/com/atguigu/springcloud/ProviderApplication.java
+++ microservicecloud-provider/src/main/java/com/atguigu/springcloud/ProviderApplication.java
@@ -3,9 +3,11 @@ package com.atguigu.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan(basePackages={"com.atguigu.springcloud.dao"})
+@EnableEurekaClient
 public class ProviderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProviderApplication.class, args);
